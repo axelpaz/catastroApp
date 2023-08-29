0631733f5e60c386bf917ba8e920f7c0507f853c
Alvaro Blanco <a.blanco@gruporamos.com>
2023-08-29 12:07:38 -0400

    proyecto creado en servidor

--- src/app/database/conexion.js
+++ src/app/database/conexion.js
@@ -1,13 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.USER,
-  process.env.PASSWORD,
-  {
-    host: process.HOSTDEV,
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(`postgres://postgres:${process.env.PASSWORD}@${process.env.HOSTPROD}:5432/${process.env.DATABASE}`,{
+dialect: 'postgres'
+})
 
 module.exports = sequelize;
